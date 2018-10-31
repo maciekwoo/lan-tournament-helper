1020cd5ef2dbcbc5a18b75096f12686bfaa33242
Michal Kujawski <michalkujawski@workfront.com>
2018-10-31 22:18:43 +0100

    is done boy

---
+++ console_interface.py
@@ -0,0 +1,152 @@
+import os
+import sys
+import logic
+
+###
+# METHOD DECLARATION
+###
+
+
+def clear_console():
+    """Clear console using system-specific command"""
+    clear_command_string = 'clear' if 'linux' in sys.platform else 'cls'
+    os.system(clear_command_string)
+
+
+def display_players_info(clear_console_prior: bool = True):
+    """Display players"""
+    if clear_console_prior:
+        clear_console()
+
+    print('Players:')
+    print(*(f'{player.id}: {player.name}' for player in controller.players), sep="\n")
+
+
+def display_games_info(clear_console_prior: bool = True):
+    """Display games"""
+    if clear_console_prior:
+        clear_console()
+
+    print('Games:')
+    print(*(f'{game.id}: {game.name}' for game in controller.games), sep="\n", end="\n")
+
+
+def display_matches_list(clear_console_prior: bool = True):
+    """Display matches"""
+    if clear_console_prior:
+        clear_console()
+
+    print("Matches:")
+    print(*controller.matches, sep="\n")
+
+
+def display_scoreboard(clear_console_prior: bool = True):
+    """Display scoreboard"""
+    if clear_console_prior:
+        clear_console()
+
+    print("Scoreboard:")
+    player_scores = controller.get_player_scores()
+    scoreboard = sorted(
+        [entry for entry in zip(player_scores.keys(), player_scores.values())], key=lambda x: x[1], reverse=True)
+
+    for entry in scoreboard:
+        print(f'{entry[0]}: {entry[1]}')
+
+
+def display_menu_options(clear_console_prior: bool = True):
+    """Display menu options - actions that can be invoked in application"""
+    if clear_console_prior:
+        clear_console()
+
+    options = ['Finalize match', 'Display scoreboard', 'Display matches']
+
+    for (index, option) in enumerate(options):
+        print(f'{index+1}: {option}')
+
+
+###
+# START OF SCRIPT LIFECYCLE
+###
+
+# Read player names and game names and amount of round to be played by each player
+player_names = input("Player names (comma separated): ").replace(' ', '').split(',')
+game_names = input("Game names (comma separated): ").replace(' ', '').split(',')
+round_count = int(input("Round count (default: 1): ") or 1)
+
+# Initialise controller
+controller = logic.TournamentController(player_names, game_names)
+
+# Gather data about all matches (who plays what game with whom etc.)
+for rounds in range(round_count):
+    for player in controller.players:
+        display_games_info()
+        print('')
+        display_players_info(False)
+        print(f'\nMatch details for Player: {player.name} ')
+
+        game_id = input('Game ID (ENTER for random): ')
+        if game_id == '':
+            game = controller.randomize_game()
+            game_id = game.id
+            print(f'Randomized game: {game}')
+        else:
+            game_id = int(game_id)
+            game = controller.get_game_by_id(game_id)
+            print(f'Selected game: {game}')
+
+        excluded_opponent_ids = [player.id]
+        all_player_ids = [p.id for p in controller.players]
+        opponent_ids = []
+
+        while len(opponent_ids) < 2:
+            opponent_id = input(f'Opponent #{len(opponent_ids)+1} ID (ENTER for random{", - for none" if len(opponent_ids) > 0 else ""}): ')
+            if opponent_id == '-':
+                if len(opponent_ids) > 0:
+                    break
+            else:
+                try:
+                    opponent_id = int(opponent_id)
+                    if (opponent_id in all_player_ids) and (opponent_id not in excluded_opponent_ids):
+                        opponent_ids.append(opponent_id)
+                        excluded_opponent_ids.append(opponent_id)
+                    else:
+                        print('Player already takes part in this match or provided value is not a correct Player ID')
+                except ValueError:
+                    randomized_opponent = controller.randomize_player(excluded_ids=excluded_opponent_ids)
+                    opponent_ids.append(randomized_opponent.id)
+                    excluded_opponent_ids.append(randomized_opponent.id)
+
+        controller.add_match(player.id, opponent_ids, game_id)
+
+# User action loop
+while True:
+    display_menu_options()
+    selected_option = int(input('Option: ') or 0)
+
+    if selected_option == 1:  # 'Finalize match'
+        display_matches_list()
+        try:
+            match_id = int(input('Match ID: '))
+            match = controller.get_match_by_id(match_id)
+            main_player_won = (input(f'{match.player.name} won? [Y/n]: ') or 'y').lower() == 'y'
+            controller.finalize_match(match_id, main_player_won)
+        except ValueError:
+            print('No match selected')
+
+    elif selected_option == 2:  # 'Display scoreboard'
+        display_scoreboard()
+
+    elif selected_option == 3:  # 'Display matches'
+        display_players_info()
+        try:
+            player_id = int(input('Player ID (ENTER for all): '))
+            player = controller.get_player_by_id(player_id)
+            main_player_only = (input(f'{player.name} only as main player? [y/N]: ') or 'n').lower() == 'y'
+            print('Matches: ', *controller.get_matches_by_player_id(player_id, main_player_only), sep='\n')
+        except ValueError:
+            display_matches_list()
+    else:
+        print('Incorrect option selected')
+
+    input('Press any key to continue...')
---
+++ logic.py
@@ -0,0 +1,148 @@
+import random
+import math
+
+
+class Player:
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+        self.score = 0
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Game:
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Match:
+    NOT_PLAYED_YET = 0
+    MAIN_PLAYER_WON = 1
+    OPPONENTS_WON = 2
+
+    def __init__(self, id: int, player: Player, game: Game, opponents: list, prize_score: int):
+        self.id = id
+        self.player = player
+        self.opponents = opponents
+        self.prize_score = prize_score
+        self.game = game
+        self.state = self.NOT_PLAYED_YET
+
+    def __str__(self):
+        opponents = str(self.opponents).strip("[]").replace(", ","+")
+        players = f'{self.player} vs {opponents}'
+
+        if self.state == self.NOT_PLAYED_YET:
+            winner = 'Not played yet'
+        elif self.state == self.MAIN_PLAYER_WON:
+            winner = self.player.name
+        else:
+            winner = opponents
+
+        return f'#{self.id}: [Players: {players}] [Game: {self.game}] [Score: {self.prize_score}] [Winner: {winner}]'
+
+
+class TournamentController:
+    SCORE_PICKED_GAME = 1
+    SCORE_RANDOM_GAME = 3
+    SCORE_PICKED_OPPONENT = 1
+    SCORE_RANDOM_OPPONENT = 3
+
+    def __init__(self, player_names: list, game_names: list):
+
+        self.games = [Game(id+1, name) for (id, name) in enumerate(game_names)]
+        self.players = [Player(id+1, name) for (id, name) in enumerate(player_names)]
+        self.matches = []
+
+    def get_game_by_id(self, game_id: int) -> Game:
+        return next((game for game in self.games if game.id == game_id), None)
+
+    def get_player_by_id(self, player_id: int) -> Player:
+        return next((player for player in self.players if player.id == player_id), None)
+
+    def get_match_by_id(self, match_id: int) -> Match:
+        return next((match for match in self.matches if match.id == match_id), None)
+
+    def get_matches_by_player_id(self, player_id: int, only_main_player: bool = False) -> list:
+        player = self.get_player_by_id(player_id)
+
+        if only_main_player:
+            return [match for match in self.matches if player == match.player]
+        else:
+            return [match for match in self.matches if (player == match.player or player in match.opponents)]
+
+    def get_players_by_match_id(self, match_id: int) -> list:
+        match = next((match for match in self.matches if match.id == match_id), None)
+        return [match.player] + match.opponents
+
+    def randomize_game(self) -> Game:
+        return random.choice(self.games)
+
+    def randomize_player(self, excluded_ids: list = None) -> Player:
+        if excluded_ids is None:
+            excluded_ids = []
+
+        return random.choice([player for player in self.players if player.id not in excluded_ids])
+
+    def add_match(self, player_id: int,  opponent_ids: list, game_id: int = None):
+
+        match_id = len(self.matches)+1
+        player = self.get_player_by_id(player_id)
+        prize_score = 0
+        opponents = []
+        excluded_opponent_ids = [player_id]
+
+        for opponent_id in opponent_ids:
+            if opponent_id:
+                opponent = self.get_player_by_id(opponent_id)
+                prize_score += self.SCORE_PICKED_OPPONENT
+            else:
+                opponent = self.randomize_player(excluded_ids=excluded_opponent_ids)
+                prize_score += self.SCORE_RANDOM_OPPONENT
+
+            excluded_opponent_ids.append(opponent.id)
+            opponents.append(opponent)
+
+        if game_id:
+            game = self.get_game_by_id(game_id)
+            prize_score += self.SCORE_PICKED_GAME
+        else:
+            game = self.randomize_game()
+            prize_score += self.SCORE_RANDOM_GAME
+
+        self.matches.append(Match(match_id, player, game, opponents, prize_score))
+
+    def finalize_match(self, match_id: int, main_player_won: bool) -> Match:
+        match = self.get_match_by_id(match_id)
+
+        if main_player_won:
+            match.state = Match.MAIN_PLAYER_WON
+        else:
+            match.state = Match.OPPONENTS_WON
+
+        return match
+
+    def get_player_scores(self) -> dict:
+        completed_matches = [match for match in self.matches if match.state != Match.NOT_PLAYED_YET]
+
+        player_scores = {player: 0 for player in self.players}
+        for match in completed_matches:
+            if match.state == Match.MAIN_PLAYER_WON:
+                player_scores[match.player] += match.prize_score
+            else:
+                for opponent in match.opponents:
+                    player_scores[opponent] += math.ceil(match.prize_score/len(match.opponents))
+
+        return player_scores
